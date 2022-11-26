63c3d35869a883f1520ab437ca69a6b968d1e0a4
youssef <yr22990@gmail.com>
2022-11-26 10:18:18 +0200

    first Commit

---
+++ HM YM YM Restaurant/script/Details.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $("button").click(function(){
+       $(".total").toggle(); 
+    });
+});
+$(document).ready(function(){
+   $(".Higo").fadeIn(3000);
+});
---
+++ HM YM YM Restaurant/script/blog.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+   $(".FT").fadeIn(3000);
+});
+$(document).ready(function(){
+   $(".navbar").slideDown(7000); 
+});
